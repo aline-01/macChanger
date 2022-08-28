1abb6eb2fde545490d48a3f59f14321b1dd6003c
aline-01 <aaryn3942@gmail.com>
2022-08-29 03:37:01 +0430

    the random mac genratore is created

--- macChanger.py
+++ macChanger.py
@@ -1,8 +1,11 @@
 import os
 import re
 import time
+import random
+import string
 from subprocess import check_output
 from argparse import ArgumentParser
+from random import choice
 
 user = check_output("whoami",shell=True)
 user = str(user)
@@ -13,7 +16,7 @@ if user != "root":
     exit()
 
 def logo():
-    print("""              _____ _
+    print("""                           _____ _
                           /  __ \ |                                 
       _ __ ___   __ _  ___| /  \/ |__   __ _ _ __   __ _  ___ _ __  
      | '_ ` _ \ / _` |/ __| |   | '_ \ / _` | '_ \ / _` |/ _ \ '__| 
@@ -22,48 +25,78 @@ def logo():
                                                    __/ |           
                                                    |___/   
          -i --interface  =  your interface
-         -m --mac  =  your new mac address
+         -r --random = generate a random MAC address 
+         -m --mac  =  enter the custom MAC addres
     """)
 
 logo()
 
-def change_mac(mac,interface):
-    os.system("sudo ifconfig " + interface + " down")
-    os.system("sudo ifconfig " + interface + " hw ether " + mac)
-    os.system("sudo ifconfig " + interface + " up")
-    print("\t [+] mac is change")
-    print("\t [+] interface: "+interface)
-    print("\t [+] new mac: "+mac)
 
 def create_argument():
     parser = ArgumentParser()
     parser.add_argument("-i","--interface",dest="interface",help="[+] give your interface with -i or --interface")
     parser.add_argument("-m","--mac",dest="mac",help="[+] give me a mac address with -m or --mac")
+    parser.add_argument("-r","--random",action="store_true",help="[+] for generate random MAC address")
     options = parser.parse_args()
     return options
 
+def create_mac():
+    uppercase = ''.join(string.hexdigits.upper())
+    mac = ""
+    for i in range(6):
+    	for j in range(2):
+    	   if i == 0:
+    	       mac += choice("02468ACE")
+    	   else:
+    	       mac += choice(uppercase)
+    	mac += ":"
+    return mac.strip(":")
+
+def current_mac():
+    ifconfig = check_output("ifconfig "+interface,shell=True)
+    current_mac = re.search("ether (.+)",str(ifconfig)).group().split()[1].strip()
+    return current_mac
+
+def change_mac(mac,current_mac,interface):
+    
+    check_output("sudo ifconfig "+interface+" down",shell=True)
+    check_output("sudo ifconfig "+interface+" hw ether "+mac,shell=True)
+    check_output("sudo ifconfig "+interface+" up",shell=True)
+    print("\t [+] mac is changed ")
+    print("\t [+] interface : "+interface)
+    print("\t [+] old mac : "+current_mac)
+    print("\t [+] new mac : "+mac)
+    print("")
+    
+	
 arguments = create_argument()
 mac = arguments.mac
 interface = arguments.interface
+random = arguments.random
 
 if interface == None:
     print("\t [!] give me your interface")
     time.sleep(2)
     exit()
 
-mac_regex = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
-if mac == None:
-    print("\t [!] give me a mac address ")
-    time.sleep(2)
-    exit()
+if random == True:
+    mac = create_mac()
 else:
-    true_mac = re.search(mac_regex,mac)
-    if true_mac == None:
-        print("\t [!] give me true mac address")
-        time.sleep(2)
-        exit()
+    if mac == None:
+       mac_regex = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
+       print("\t [!] give me a mac address ")
+       time.sleep(2)
+       exit()
+    else:
+       true_mac = re.search(mac_regex,mac)
+       if true_mac == None:
+          print("\t [!] give me true mac address")
+          time.sleep(2)
+          exit()
 
-change_mac(mac,interface)
+current_mac = current_mac()
 
-print("it's must be update")
+change_mac(mac,current_mac,interface)
+
+
 
